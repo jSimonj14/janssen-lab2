df7de030222024fb029858a74d7919d96e6d3258
jSimonj14 <141962382+jSimonj14@users.noreply.github.com>
2024-02-27 19:15:36 -0700

    janssen-lab2

---
+++ sevenSegDecoder_1.srcs/sim_1/new/sevenSegDecoder_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/27/2024 06:00:28 PM
+// Design Name: 
+// Module Name: sevenSegDecoder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sevenSegDecoder_tb(
+
+    );
+endmodule
